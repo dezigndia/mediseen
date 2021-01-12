14c1b047a6cd8e69b1d47582799b3ebc9b29f049
harshgoel05 <harshgoel05@gmail.com>
2021-01-12 17:32:44 +0530

    environment setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,8 @@
 const app = require("./app")
+const config = require("config")
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}.`))
+const PORT = config.has("database.app.port") ? config.get("database.app.port") : 5000
+app.listen(PORT, () => {
+  console.log("MODE:", process.env.NODE_ENV)
+  console.log(`Server running on port ${PORT}.`)
+})
