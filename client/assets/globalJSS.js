bda5b88ab8d1c1446bb05d9946ee39c40f5a4032
AmKreta <41804653+AmKreta@users.noreply.github.com>
2021-01-14 12:21:49 +0530

    Add files via upload

---
+++ client/assets/globalJSS.js
@@ -0,0 +1,13 @@
+export const blue = '#5C4DB1';
+export const lightBlue = '#2BBEC8';
+export const purple = '#220555';
+export const pink = '#EC2B7A';
+export const green = '#1EBE71';
+export const lightGreen = '#1DE9B6';
+export const brown = '#D65246';
+export const darkGreen = '#128C7e';
+export const blueGradient = 'lineargradient(to right, #2BBEC8, #1DE9B6)';
+export const pinkGradient = 'lineargradient(to right, #220555, #EC2B7A)';
+
+export const medium = '500px';
+export const large = '768px';
