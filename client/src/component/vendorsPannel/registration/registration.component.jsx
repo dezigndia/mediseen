b8806541cf7d6f06a7049e1966fb66394d656f11
yash1798 <68080363+yash1798@users.noreply.github.com>
2021-02-02 12:02:41 +0530

    yash

--- client/src/component/vendorsPannel/registration/registration.component.jsx
+++ client/src/component/vendorsPannel/registration/registration.component.jsx
@@ -1,52 +1,72 @@
-import React, { useState } from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import './registration.styles.scss';
+import React, { useState } from "react"
+import { Switch, Route, Redirect } from "react-router-dom"
+import "./registration.styles.scss"
 
 //custom Component
-import Header from '../header/header.component';
-import Footer from '../footer/footer.component';
-import WelcomeOtpScreen from './welcomeOtpScreen/welcomeOtpScreen.component';
-import BusinessInfoForm from './businessInfoForm/businessInfoForm.component';
-import RegisterAs from './registerAs/registerAs.component';
-import RegisterAsDoctor from './registerAsDoctor/registerAsDoctor.component';
-import RegisterAsHospital from './registerAsHospital/registerAsHospital.component';
-import RegisterAsPharmacy from './registerAsPharmacy/registerAsPharmacy.component';
-import RegisterAsPathology from './registerAsPathology/rtegisterAsPathology.component';
+import Header from "../header/header.component"
+import Footer from "../footer/footer.component"
+import WelcomeOtpScreen from "./welcomeOtpScreen/welcomeOtpScreen.component"
+import BusinessInfoForm from "./businessInfoForm/businessInfoForm.component"
+import RegisterAs from "./registerAs/registerAs.component"
+import RegisterAsDoctor from "./registerAsDoctor/registerAsDoctor.component"
+import RegisterAsHospital from "./registerAsHospital/registerAsHospital.component"
+import RegisterAsPharmacy from "./registerAsPharmacy/registerAsPharmacy.component"
+import RegisterAsPathology from "./registerAsPathology/rtegisterAsPathology.component"
 
 //importing Routes
 import {
-    REGISTER_AS,
-    ADD_BUSINESS_INFO,
-    REGISTER_AS_DOCTOR,
-    REGISTER_AS_HOSPITAL,
-    REGISTER_AS_PATHOLOGY,
-    REGISTER_AS_PHARMACY
-} from './routes';
+	REGISTER_AS,
+	ADD_BUSINESS_INFO,
+	REGISTER_AS_DOCTOR,
+	REGISTER_AS_HOSPITAL,
+	REGISTER_AS_PATHOLOGY,
+	REGISTER_AS_PHARMACY,
+} from "./routes"
 
 const Registration = ({ match }) => {
-    const [isRegistered, setIsRegistered] = useState(false);
-    return (
-        <div className="registrationContainer">
-            {
-                isRegistered
-                    ? <>
-                        <Header />
-                        <Switch>
-                            <Route path={`${match.url}/${REGISTER_AS}`} component={RegisterAs} />
-                            <Route path={`${match.url}/${REGISTER_AS_DOCTOR}`} component={RegisterAsDoctor} />
-                            <Route path={`${match.url}/${REGISTER_AS_HOSPITAL}`} component={RegisterAsHospital} />
-                            <Route path={`${match.url}/${REGISTER_AS_PHARMACY}`} component={RegisterAsPharmacy} />
-                            <Route path={`${match.url}/${REGISTER_AS_PATHOLOGY}`} component={RegisterAsPathology} />
-                        </Switch>
-                        <Footer />
-                    </>
-                    : <Switch>
-                        <Route exact path={`${match.url}/`} component={WelcomeOtpScreen} />
-                        <Route path={`${match.url}/${ADD_BUSINESS_INFO}`} render={({ history, match }) => <BusinessInfoForm {...{ setIsRegistered, history, match }} />} />
-                    </Switch>
-            }
-        </div>
-    );
+	const [isRegistered, setIsRegistered] = useState(false)
+	return (
+		<div className="registrationContainer">
+			{isRegistered ? (
+				<>
+					<Header />
+					<Switch>
+						<Route
+							path={`${match.url}/${REGISTER_AS}`}
+							component={RegisterAs}
+						/>
+						<Route
+							path={`${match.url}/${REGISTER_AS_DOCTOR}`}
+							component={RegisterAsDoctor}
+						/>
+						<Route
+							path={`${match.url}/${REGISTER_AS_HOSPITAL}`}
+							component={RegisterAsHospital}
+						/>
+						<Route
+							path={`${match.url}/${REGISTER_AS_PHARMACY}`}
+							component={RegisterAsPharmacy}
+						/>
+						<Route
+							path={`${match.url}/${REGISTER_AS_PATHOLOGY}`}
+							component={RegisterAsPathology}
+						/>
+					</Switch>
+					<Footer />
+				</>
+			) : (
+				<Switch>
+					<Route exact path={`${match.url}/`} component={WelcomeOtpScreen} />
+					<Route
+						path={`${match.url}/${ADD_BUSINESS_INFO}`}
+						render={({ history, match }) => (
+							<BusinessInfoForm {...{ setIsRegistered, history, match }} />
+						)}
+					/>
+				</Switch>
+			)}
+		</div>
+	)
 }
 
-export default Registration;
+export default Registration
