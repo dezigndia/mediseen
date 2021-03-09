5a3f31a0305fa429b5ce50a1a60f45e4f8c79c03
harshgoel05 <harshgoel05@gmail.com>
2021-03-09 13:22:32 +0530

    update work schema

--- server/src/models/Schemas/workTimings.js
+++ server/src/models/Schemas/workTimings.js
@@ -2,25 +2,25 @@ const mongoose = require("mongoose")
 
 const daySchema = new mongoose.Schema({
     _id: false,
-    morning:{
-        from: {type: String},
-        to: { type: String},
+    morning: {
+        from: { type: Number },
+        to: { type: Number },
     },
-    evening:{
-        from: {type: String},
-        to: { type: String},
-    }
+    evening: {
+        from: { type: Number },
+        to: { type: Number },
+    },
 })
 
 const workSchema = new mongoose.Schema({
     _id: false,
-    Monday:{type:daySchema},
-    Tuesday:{type: daySchema},
-    Wednesday: {type: daySchema},
-    Thursday: {type: daySchema},
-    Friday: { type: daySchema},
-    Saturday:{type: daySchema},
-    Sunday:{type: daySchema}
+    Monday: { type: daySchema },
+    Tuesday: { type: daySchema },
+    Wednesday: { type: daySchema },
+    Thursday: { type: daySchema },
+    Friday: { type: daySchema },
+    Saturday: { type: daySchema },
+    Sunday: { type: daySchema },
 })
 
 module.exports = workSchema
