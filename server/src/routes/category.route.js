82269171ee1bd811ddc9ede77f70bd2e69c3e979
harshgoel05 <harshgoel05@gmail.com>
2021-03-23 13:19:33 +0530

    change aws cred

--- server/src/routes/category.route.js
+++ server/src/routes/category.route.js
@@ -9,8 +9,3 @@ router.get("/type/:type", authenticate(), cc.getCategoriesByType)
 router.post("/", authenticate(), cc.createCategory)
 
 module.exports = router
-
-
-// GET=> /category/type/:type
-
-// POST => 
