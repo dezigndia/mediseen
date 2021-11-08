5c0e7385ab34aad32002dd8f1193d8750081495c
rakesh-kumar-pradhan <rakeshkp683@gmail.com>
2021-11-08 17:21:38 +0530

    updated!!

--- admin-panel/src/views/Orders.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import OrdersContent from "./orders/OrdersContent";
-export default function Orders() {
-  return (
-    <div className="content">
-      <OrdersContent />
-    </div>
-  );
-}
--- admin-panel/src/views/Products.js
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState } from "react";
-import ProductsTable from "./products/ProductsTable";
-export default function Users() {
-  return (
-    <div className="content">
-      <ProductsTable />
-    </div>
-  );
-}
